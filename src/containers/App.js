65707d6eaef881b224fa43bcb4f7fad52ef2cda6
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2022-08-15 04:53:13 +0200

    Added redux to package

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,59 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import { setSearchField, requestRobots } from "../actions";
 
-const App = () => {
-	const [filteredRobots, setFilteredRobots] = useState([]);
-	const [hasLoaded, setHasLoaded] = useState(false);
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+	};
+};
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots()),
+	};
+};
+
+const App = (props) => {
+
+	const { searchField, onSearchChange, robots, isPending } = props;
+
+	console.log(props);
 
 	useEffect(() => {
-		if (!hasLoaded) {
-			fetchUsers();
-		}
+		props.onRequestRobots();
+	}, []);
+
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	});
 
-	const fetchUsers = () => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((response) => {
-				return response.json();
-			})
-			.then((users) => {
-				setFilteredRobots(users);
-				setHasLoaded(true);
-			});
-	};
-
-	const onSearchChange = (event) => {
-		event.target.value === ""
-			? fetchUsers()
-			: getFilteredRobots(event.target.value);
-	};
-
-	const getFilteredRobots = (searchField) => {
-		setFilteredRobots(
-			filteredRobots.filter((robot) => {
-				return robot.name
-					.toLowerCase()
-					.includes(searchField.toLowerCase());
-			})
-		);
-	};
-
-	return (
+	return isPending ? (
+		<h1>Loading...</h1>
+	) : (
 		<div className="tc">
 			<h1 className="f1">RoboFriends</h1>
 			<SearchBox searchChange={onSearchChange} />
 			<Scroll>
-				{filteredRobots.length === 0 ? (
-					<h1>Loading...</h1>
-				) : (
-					<CardList robots={filteredRobots} />
-				)}
+				<CardList robots={filteredRobots} />
 			</Scroll>
 		</div>
 	);
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
